b3cdd211fe7d099c8931ee40a88a43254fb7d571
Victo-2137506 <2137506@carrefour.cegepvicto.ca>
2025-11-27 18:30:49 -0500

    test

--- src/common/constants/ENV.ts
+++ src/common/constants/ENV.ts
@@ -11,7 +11,7 @@ const ENV = jetEnv({
   NodeEnv: isEnumVal(NodeEnvs),
   Port: num,
   Mongodb: str,
-  Jwtsecret: str,
+  JWTSECRET: str,
 });
 
 /******************************************************************************
